e139cb26e99598ca7d8288b41052b963ec0b572a
Ming Cheung <chi.m.cheung@intel.com>
2021-01-12 14:09:19 -0800

    Removed experimental pipelines.
    Signed-off-by: Ming Cheung <chi.m.cheung@intel.com>

--- inventory/src/integration/resources/scripts/teardown_testbed.sh
+++ inventory/src/integration/resources/scripts/teardown_testbed.sh
@@ -3,4 +3,5 @@ set -ex
 
 rm -f $etcDir/HWInvDiscoveryConfig.json"
 docker-compose -f $scriptDir/json-server/json-server.yml down" || true
+mkdir -p $tmpDir
 source $scriptDir/stop-voltdb.sh >> $tmpDir/voltdb.log
